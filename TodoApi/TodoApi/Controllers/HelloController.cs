b2b86f97e04076a0a9243e7c11809799e0430db2
snyadav1994 <syadav@tavisca.com>
2018-05-09 10:22:35 +0530

    First commit

---
+++ TodoApi/TodoApi/Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class HelloController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World";
+        }
+
+    }
+}
